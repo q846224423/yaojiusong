f3ac18907eca9f99589420b887092091cefc03ad
q846224423 <846224423@qq.com>
2019-05-21 10:52:08 +0800

    范鉴涛修改

--- java01/java01/Deemo.java
+++ java01/java01/Deemo.java
@@ -1,8 +1,8 @@
 package java01;
 
 public class Deemo {
-	private void sysout() {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
 		System.out.println("123");
 	}
 }
+
