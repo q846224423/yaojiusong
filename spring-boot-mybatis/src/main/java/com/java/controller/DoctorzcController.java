ce3e72adf1b9f9132dde0231af0df0783b882a5f
chen <chen@DESKTOP-PJ8HBVN>
2019-05-22 10:45:38 +0800

    1

--- spring-boot-mybatis/src/main/java/com/java/controller/DoctorzcController.java
+++ spring-boot-mybatis/src/main/java/com/java/controller/DoctorzcController.java
@@ -6,11 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DoctorzcController {
-
+//这里跳转到医生注册界面
 @RequestMapping("/doctorzc")
 public String  doctorzc() {
 	
 
 	return "doctorzc";	  
-} 
+}
+
+
+//这里跳转到医生个人信息界面
+@RequestMapping("/doctoruser")
+public String doctoruser() {
+	
+	
+	
+	return null;	
+}
+
+
+
 }
