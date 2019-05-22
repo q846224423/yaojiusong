95285cc4a7bfb8264c9272035388c2120cc6e80c
chen <chen@DESKTOP-PJ8HBVN>
2019-05-22 15:17:44 +0800

    c

--- spring-boot-mybatis/src/main/java/com/java/controller/DoctorController.java
+++ spring-boot-mybatis/src/main/java/com/java/controller/DoctorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class DoctorzcController {
+public class DoctorController {
 //这里跳转到医生注册界面
 @RequestMapping("/doctorzc")
 public String  doctorzc() {
