194554e7db299696a967a7abc197e3a0ec7315c2
Administrator <Administrator@EGL3BS24N759D9N>
2019-05-23 10:29:47 +0800

    备份

--- spring-boot-mybatis/src/main/java/com/java/mapper/StoreMapper.java
+++ spring-boot-mybatis/src/main/java/com/java/mapper/StoreMapper.java
@@ -2,19 +2,26 @@ package com.java.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
 import com.java.pojo.Menu1;
 import com.java.pojo.Menu2;
 import com.java.pojo.Menu3;
-
+@Repository
 public interface StoreMapper {
 	/**@主页面的菜单
 	 */
 	//所有1级药类
+	@Select("select * from menuone")
 	public List<Menu1> selectA1();
 	//通过1级id找二级
+	@Select("select * from menutwo where menu2_menu1_id=#{id}")
 	public List<Menu2> select2By1id(int id);
 	//通过2级id找三级
+	@Select("select * from menuthree where menu3_menu2_id=#{id}")
 	public List<Menu3> select3By2id(int id);
+	
 	//获取左侧三级菜单栏集合
 	public List<Menu1> getAllMenu();
 	
@@ -22,12 +29,15 @@ public interface StoreMapper {
 	 * @各种菜单的点击查询
 	 */
 	//搜索框来模糊查询
+	@Select("select * from menuthree where menu3_name like concat('%',#{name},'%')")
 	public List<Menu3> select3ByName(String name);
 	//通过1级查询所有商品
+	@Select("select `menu3_id`,`menu3_menu2_id`,`menu3_name`,`ep_price`,`ep_size`,`ep_description`,`ep_stock`,`ep_url`,`ep_stick`,`yd_id` from view1  where menu1_id=#{id}")
 	public List<Menu3> select3By1id(int id);
 	//通过2级查询所有商品  select3By2id
-	//通过3级查询所有商品
-	public List<Menu3> select3By3id(int id);
+	//通过3级查询商品
+	@Select("select * from menuthree where menu3_id=#{id}")
+	public Menu3 select3By3id(int id);
 	
 	/**
 	 * @商户的个人信息
