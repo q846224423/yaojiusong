6d8beef7e97f173eeab2317c0fcdce923d88aedb
chen <chen@DESKTOP-PJ8HBVN>
2019-05-23 10:29:15 +0800

    c

--- spring-boot-mybatis/src/main/java/com/java/controller/CommonControl.java
+++ spring-boot-mybatis/src/main/java/com/java/controller/CommonControl.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CommonControl {
 	
 		//这里跳转到医生注册界面
-		@RequestMapping("/doctorzc")
+		@RequestMapping("/doctrzc")
 		public String  doctorzc() {
 			
 
-			return "loginb";	  
+			return "doctorzc";	  
 		}
 		
 		
