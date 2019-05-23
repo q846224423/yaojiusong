4695936d175f8894023d7b9e357272137c6e8992
Aliome <Aliome@Aliome-PC>
2019-05-23 09:51:08 +0800

    pojo

---
+++ spring-boot-mybatis/src/main/java/com/java/pojo/Kb.java
@@ -0,0 +1,26 @@
+package com.java.pojo;
+
+public class Kb {
+	private int kb_id;
+	private String kb_name;
+	private int ks_id;
+	public int getKb_id() {
+		return kb_id;
+	}
+	public void setKb_id(int kb_id) {
+		this.kb_id = kb_id;
+	}
+	public String getKb_name() {
+		return kb_name;
+	}
+	public void setKb_name(String kb_name) {
+		this.kb_name = kb_name;
+	}
+	public int getKs_id() {
+		return ks_id;
+	}
+	public void setKs_id(int ks_id) {
+		this.ks_id = ks_id;
+	}
+	
+}
---
+++ spring-boot-mybatis/src/main/java/com/java/pojo/Ks.java
@@ -0,0 +1,19 @@
+package com.java.pojo;
+
+public class Ks {
+	private int ks_id;
+	private String ks_name;
+	public int getKs_id() {
+		return ks_id;
+	}
+	public void setKs_id(int ks_id) {
+		this.ks_id = ks_id;
+	}
+	public String getKs_name() {
+		return ks_name;
+	}
+	public void setKs_name(String ks_name) {
+		this.ks_name = ks_name;
+	}
+	
+}
