9fdb40e6d0c65055b18306cce8ffff6d7d646a21
chen <chen@DESKTOP-PJ8HBVN>
2019-05-23 09:39:03 +0800

    c

--- spring-boot-mybatis/src/main/java/com/java/mapper/DoctorMapper.java
+++ spring-boot-mybatis/src/main/java/com/java/mapper/DoctorMapper.java
@@ -1,12 +1,29 @@
 package com.java.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
+
+import com.java.pojo.Doctor;
 @Repository
 public interface DoctorMapper {
-	//查询账号密码
-	@Select("select * from people")
-	public void selectdoctorall();
-	
-
+    //医生查询所有
+    @Select("select * from doctor")
+    List<Doctor> selectall();
+    //医生查询单条
+    @Select("select * from doctor where id=#{id}")
+    Doctor selectone(int id);
+    //医生修改数据
+    @Update("update doctor set age=#{age},name=#{name} where id=#{id}")
+    int update(Doctor doctor);
+    //医生添加数据
+    @Insert("insert into doctor( `age`, `name`) values(#{age}, #{name})")
+    int insert(Doctor doctor);
+    //医生删除数据
+    @Delete("delete from doctor where id = #{id}")
+    int delete(int id);
 }
--- spring-boot-mybatis/src/main/java/com/java/service/Doctorservice.java
+++ spring-boot-mybatis/src/main/java/com/java/service/Doctorservice.java
@@ -1,9 +1,20 @@
 package com.java.service;
 
+import java.util.List;
+
+import com.java.pojo.Doctor;
 
 public interface Doctorservice {
-	
-	//查询账号密码
-	public void selectdoctorall();
+
+    //查询所有
+    public List<Doctor> selectall();
+    //查询单条
+    public Doctor selectone(int id);
+    //修改数据
+    public int update(Doctor doctor);
+    //添加数据
+    public int insert(Doctor doctor);
+    //删除数据
+    public int delete(int id);
 
 }
