238a7187ea8f99a7828fde9e09f5af40fcf2d1a2
q846224423 <846224423@qq.com>
2019-05-25 16:44:21 +0800

    123

--- spring-boot-mybatis/src/main/java/com/java/mapper/DrugstoreMapper.java
+++ spring-boot-mybatis/src/main/java/com/java/mapper/DrugstoreMapper.java
@@ -6,23 +6,27 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-
 import com.java.pojo.Doctor;
 import com.java.pojo.DrugStore;
 
 public interface DrugstoreMapper {
+	
     //门店查询所有
     @Select("select * from drugstore")
     List<Doctor> dsselectall();
+    
     //门店查询单条
     @Select("select * from drugstore where yd_id=#{0}")
     Doctor dsselectone(int id);
+    
     //门店修改数据
     @Update("update `shop1`.`drugstore` set `yd_name` = #{yd_name},`county_id` = #{county_id},`comment` = #{comment},`regist_info` = #{regist_info},`yd_statu` = #{yd_statu},`people_id` = #{people_id} where `yd_id` = #{yd_id}")
     int dsupdate(DrugStore drugStore);
+    
     //门店添加数据
     @Insert("insert into `shop1`.`drugstore` (`yd_name`,`county_id`,`comment`,`regist_info`,`yd_statu`,`people_id`) values(#{yd_name},#{county_id},#{comment},#{regist_info},#{yd_statu},#{people_id})")
     int dsinsert(DrugStore drugStore);
+    
     //门店删除数据
     @Delete("delete from drugstore where yd_id = #{id}")
     int dsdelete(int id);	
