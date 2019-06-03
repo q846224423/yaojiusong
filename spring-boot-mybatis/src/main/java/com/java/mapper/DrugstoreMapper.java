3d25cea48c0d99b38444ef3ed198d04f14827104
Administrator <Administrator@EGL3BS24N759D9N>
2019-06-03 16:34:41 +0800

    yb2

--- spring-boot-mybatis/src/main/java/com/java/mapper/DrugstoreMapper.java
+++ spring-boot-mybatis/src/main/java/com/java/mapper/DrugstoreMapper.java
@@ -141,5 +141,6 @@ public interface DrugstoreMapper {
 	@Update("update drugstore set yd_statu ='认证中' where yd_statu = '未认证' and yd_id = #{id}")
 	public void storeChangerz(int id);
 	
-
+	//查看非处方订单的记录
+	
 }
